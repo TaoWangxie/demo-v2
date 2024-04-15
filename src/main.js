cf414731b8c00e02b0d5e214ddb8fd7e4edf5a56
TaoWangxie <17854121873@163.com>
2024-04-15 10:00:35 +0000

    11

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import { init } from "@js-wtao/browser";
-// import { vuePlugin } from "@js-wtao/vue";
+import { init } from "@js-wtao/browser";
+import { vuePlugin } from "@js-wtao/vue";
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import dragSelect from '@/directives/dragSelect';
 
 Vue.directive('drag-select', dragSelect);
 
-// window.wtjsInstance = init({
-//     // set debug true to convenient debugger in dev,set false in prod
-//     debug: true,
-//     silentConsole: true,
-//     vue: Vue,
-//     dsn: 'https://test.com/yourInterface',
-//     maxBreadcrumbs: 100
-// },[vuePlugin])
+window.wtjsInstance = init({
+    // set debug true to convenient debugger in dev,set false in prod
+    debug: true,
+    silentConsole: true,
+    vue: Vue,
+    dsn: 'https://test.com/yourInterface',
+    maxBreadcrumbs: 100
+},[vuePlugin])
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false
